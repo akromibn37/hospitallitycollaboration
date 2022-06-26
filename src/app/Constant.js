d3e6a5ed20506eb7f16b5df47740c741a71e9b39
Akrom <akromibn37@gmail.com>
2022-06-26 12:09:49 +0700

    edit constant base_url

--- src/app/Constant.js
+++ src/app/Constant.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'http://192.168.68.104:8000';
-const EXISTING_URL = 'http://localhost:8000';
+const BASE_URL = 'http://89.47.167.214:8000';
+const EXISTING_URL = 'http://89.47.167.214:8000';
 export { BASE_URL, EXISTING_URL };
